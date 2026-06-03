0f75d09840511ac5d3e84825bd4fec700a1e1de4
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-04 04:01:51 +0900

    エントリーポイントの追加

--- Masicalan.Cli/Program.fs
+++ Masicalan.Cli/Program.fs
@@ -1,6 +1,10 @@
 ﻿open FParsec
 open Masicalan.Core
-
+open System
+open System.Diagnostics
+open System.IO
+open System.Reflection
+open System.Runtime.InteropServices
 
 let printAst script =
     match run Parser.parseProgram script with
@@ -15,3 +19,23 @@ let runInterpreter script =
 let runInterprAndPrintEnv script =
     let env = Interpreter.Run script
     printfn "%A" env
+
+// The Main Entry as follow:
+
+let assemblyFilePath = Assembly.GetExecutingAssembly().Location
+let baseDirectory = AppDomain.CurrentDomain.BaseDirectory
+let sampleCodesDirectory = Path.Combine(baseDirectory, "Samples")
+
+let appName = FileVersionInfo.GetVersionInfo(assemblyFilePath).ProductName
+let appVersion = FileVersionInfo.GetVersionInfo(assemblyFilePath).ProductVersion
+let developerName = FileVersionInfo.GetVersionInfo(assemblyFilePath).CompanyName
+let copyright = FileVersionInfo.GetVersionInfo(assemblyFilePath).LegalCopyright
+let systemVersion = Environment.OSVersion.ToString()
+let runtimeVersion = RuntimeInformation.FrameworkDescription
+
+printfn "---------------------------------------------------------------------------"
+printfn "%s ver.%s" appName appVersion
+printfn "%s" copyright
+printfn "%s" systemVersion
+printfn "%s" runtimeVersion
+printfn "---------------------------------------------------------------------------"
