da59674430dea308ca04080d2c3d1cdb479bc43a
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-06 08:23:03 +0900

    関数宣言の接頭辞を変更

--- Masicalan.Core/Parser.fs
+++ Masicalan.Core/Parser.fs
@@ -119,7 +119,7 @@ module Parser =
     
     // 関数定義文パーサ
     let parseFunction : Parser<Statement, unit> =
-        let fHead = pstring "function" >>. wspace >>. parseIdentText .>> spaces
+        let fHead = pstring "fun" >>. wspace >>. parseIdentText .>> spaces
         let fParams = between (pstring "(" .>> spaces) (pstring ")" .>> spaces) parseParams
 
         tuple3 fHead fParams (spaces >>. parseBlock)
