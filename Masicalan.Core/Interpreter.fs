fb717c3f849ed6fa5f8fb6ae5158bb12fbfdc26e
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-05 14:13:09 +0900

    Interpreter.Run 修正

--- Masicalan.Core/Interpreter.fs
+++ Masicalan.Core/Interpreter.fs
@@ -7,6 +7,10 @@ module Interpreter =
     let Run (script: string) =
         match run Parser.parseProgram script with
         | Success(ast, _, _) ->
-            List.fold Evaluator.executeStatement Map.empty ast
+            let emptyEnv : Evaluator.EnvironmentState = { VariablesEnv = Map.empty; FunctionsEnv = Map.empty}
+            List.fold 
+                (fun env stmt ->
+                    let evalRes = Evaluator.executeStatement env stmt
+                    evalRes.Environment) emptyEnv ast
         | Failure(error, _, _) ->
             failwithf "failed: %s" error
