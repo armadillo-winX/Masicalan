644c48d5387bf74def676b303cc24e2138562af4
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-06 13:01:58 +0900

    Expression を変更

--- Masicalan.Core/AstNode.fs
+++ Masicalan.Core/AstNode.fs
@@ -17,7 +17,7 @@ type Operator = Add | Sub | Mul | Div | Pow | LessThan | GreaterThan | EqualTo |
 
 // Expression : 式: 評価(evaluate)することで値になる
 type Expression =
-    | Num of int                                            // number literal
+    | ValueLit of Value                                     // value literal
     | Var of string                                         // variable reference
     | Binary of Expression * Operator * Expression          // binary operation
     | CallF of string * Expression list                     // call function
