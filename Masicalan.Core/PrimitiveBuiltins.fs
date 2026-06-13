840bfc485ef745f102e250eb7e9acdb86449bebb
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-13 18:14:02 +0900

    printFunc実装

--- Masicalan.Core/PrimitiveBuiltins.fs
+++ Masicalan.Core/PrimitiveBuiltins.fs
@@ -2,3 +2,10 @@ namespace Masicalan.Core
 
 module PrimitiveBuiltins =
     
+    let printFunc (args: Value list) =
+        match args with
+        |[Value.StringVal s] ->
+            printfn "%s" s |> ignore
+            Value.VoidVal
+        |_-> 
+            failwithf "Cannot print non-string value"
