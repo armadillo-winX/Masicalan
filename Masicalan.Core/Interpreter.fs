367c9cb4156e8537bcfc7b573873ad72bbcfe02f
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-13 18:24:57 +0900

    Intepreter の初期環境を修正

--- Masicalan.Core/Interpreter.fs
+++ Masicalan.Core/Interpreter.fs
@@ -7,7 +7,7 @@ module Interpreter =
     let Run (script: string) =
         match run Parser.parseProgram script with
         | Success(ast, _, _) ->
-            let emptyEnv : Evaluator.EnvironmentState = { VariablesEnv = Map.empty; FunctionsEnv = Map.empty}
+            let emptyEnv : Evaluator.EnvironmentState = { VariablesEnv = Map.empty; FunctionsEnv = PrimitiveBuiltins.create()}
             List.fold 
                 (fun env stmt ->
                     let evalRes = Evaluator.executeStatement env stmt
