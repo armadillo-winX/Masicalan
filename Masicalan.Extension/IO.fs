64c835e38a4508ef80310dd6f17b55ef6cb0c40b
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-17 22:33:28 +0900

    fileExistsExtFunc 追加

--- Masicalan.Extension/IO.fs
+++ Masicalan.Extension/IO.fs
@@ -5,3 +5,10 @@ open System.IO
 
 module IO =
     
+    // fileExists(string fileName)
+    let private fileExistsExtFunc (args: Value list) =
+        match args with
+        |[Value.StringVal fileName] ->
+            File.Exists(fileName) |> Value.BoolVal
+        |_-> 
+            failwithf "fileExists: arguments error"
