6ddcac655b174a0d2099c437ac59239581767dff
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-17 22:42:38 +0900

    writeToFileExtFunc 実装

--- Masicalan.Extension/IO.fs
+++ Masicalan.Extension/IO.fs
@@ -12,3 +12,12 @@ module IO =
             File.Exists(fileName) |> Value.BoolVal
         |_-> 
             failwithf "fileExists: arguments error"
+
+    // writeToFile(filePath, data)
+    let private writeToFileExtFunc (args: Value list) =
+        match args with
+        |[Value.StringVal filePath; Value.StringVal data] ->
+            File.WriteAllText(filePath, data)
+            Value.VoidVal
+        |_->
+            failwithf "writeToFile: arguments error"
