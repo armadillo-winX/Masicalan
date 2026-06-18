bbe5688a4ee9c2c9335b06e51ab277514be54144
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-18 18:49:05 +0900

    RunWithExt 実装

--- Masicalan.Core/Interpreter.fs
+++ Masicalan.Core/Interpreter.fs
@@ -14,3 +14,16 @@ module Interpreter =
                     evalRes.Environment) emptyEnv ast
         | Failure(error, _, _) ->
             failwithf "failed: %s" error
+
+    let RunWithExt (script: string) (extEnv: Map<string, (string list * Statement)>) =
+        match run Parser.parseProgram script with
+        | Success(ast, _, _) ->
+            let primEnv = PrimitiveBuiltins.create()
+            let initFuncEnv = Map.fold (fun acc key value -> Map.add key value acc) primEnv extEnv
+            let initEnv : Evaluator.EnvironmentState = { VariablesEnv = Map.empty; FunctionsEnv = initFuncEnv }
+            List.fold
+                (fun env stmt ->
+                    let evalRes = Evaluator.executeStatement env stmt
+                    evalRes.Environment) initEnv ast
+        | Failure(error, _, _) ->
+            failwithf "failed: %s" error
