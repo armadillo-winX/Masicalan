ea0bc23123ba682bd9ae11f8da3f7a8760172280
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-19 10:54:42 +0900

    startExFunc 実装

--- Masicalan.Extension/ProcessExtension.fs
+++ Masicalan.Extension/ProcessExtension.fs
@@ -1,6 +1,22 @@
 namespace Masicalan.Extension
 
+open Masicalan.Core
+open System.IO
 open System.Diagnostics
 
 module ProcessExtension =
-    
+
+    // start
+    let private startExFunc (args: Value list) =
+        match args with
+        | [Value.StringVal filePath] ->
+            let dir = Path.GetDirectoryName(filePath)
+            let psi = new ProcessStartInfo()
+            psi.FileName <- filePath
+            psi.WorkingDirectory <- dir
+            psi.UseShellExecute <- true
+            Process.Start(psi)
+            |> fun p -> p.Id
+            |> Value.IntVal
+        |_->
+            failwithf "start: arguments error"
