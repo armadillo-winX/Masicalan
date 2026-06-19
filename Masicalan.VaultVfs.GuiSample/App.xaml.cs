70b595511f7f72ccb307a46afb515c91c04019bf
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-20 04:34:36 +0900

    Masicalan.VaultVfs.GuiSample 追加

---
+++ Masicalan.VaultVfs.GuiSample/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Masicalan.VaultVfs.GuiSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
