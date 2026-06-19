7e2a00bb7ec2d26312936600daad377bdcfac7a0
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-20 00:19:27 +0900

    名前修正

--- Masicalan.VaultVfs/VfsConstants.fs
+++ Masicalan.VaultVfs/VfsConstants.fs
@@ -6,4 +6,4 @@ module VfsConstans =
     // Constants used for MASIV file format and DPAPI
     let internal HeaderMagic = "MASIV"
     let internal VersionByte = byte 1
-    let internal Entropy = Encoding.UTF8.GetBytes("Masicalan.VaultVfs:entropy:v1")
+    let internal DefaultEntropy = Encoding.UTF8.GetBytes("Masicalan.VaultVfs:entropy:v1")
