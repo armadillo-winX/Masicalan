db526bc56694d9bb9d1071822a2b1fde19b47618
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-20 00:27:53 +0900

    DefaultEntropyName 追加

--- Masicalan.VaultVfs/VfsConstants.fs
+++ Masicalan.VaultVfs/VfsConstants.fs
@@ -6,4 +6,5 @@ module VfsConstants =
     // Constants used for MASIV file format and DPAPI
     let internal HeaderMagic = "MASIV"
     let internal VersionByte = byte 1
-    let internal DefaultEntropy = Encoding.UTF8.GetBytes("Masicalan.VaultVfs:entropy:v1")
+    let internal DefaultEntropyName = "Masicalan.VaultVfs:entropy:v1"
+    let internal DefaultEntropy = Encoding.UTF8.GetBytes(DefaultEntropyName)
