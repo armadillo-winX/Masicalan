9d494106a9e0bdd0b7bea3c1ecc66abeb01cfc3c
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-20 13:56:48 +0900

    TextViewerWindow 追加

---
+++ Masicalan.VaultVfs.GuiSample/TextViewerWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Masicalan.VaultVfs.GuiSample
+{
+    /// <summary>
+    /// TextViewerWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TextViewerWindow : Window
+    {
+        public TextViewerWindow(string content)
+        {
+            InitializeComponent();
+
+            MainTextBox.Text = content;
+        }
+    }
+}
